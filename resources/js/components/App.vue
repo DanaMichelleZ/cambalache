7a4d718d84fecc80ae10a32b667c0c6e77c6202f
DanaMichelleZ <dana.zambelli@davinci.edu.ar>
2025-05-28 12:20:07 -0300

    Inicio del proyecto Cambalache con Laravel 11 y Vue 3.4

---
+++ resources/js/components/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1 style="text-align:center; font-family:sans-serif;">¡Hola desde Cambalache con Vue 3.4!</h1>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App'
+  }
+  </script>
+  
